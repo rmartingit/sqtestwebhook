1ccce23ab289a478ed20cb2d56e6bca8733127e4
Ricardo Martinho <rmartin.estg@gmail.com>
2018-04-05 16:39:22 +0100

    git pull complete from deploy.php

--- hello.php
+++ hello.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World for staging to test webhook!</p>'; ?> 
+ <?php echo '<p>Hello World for staging to test webhook, git with username!</p>'; ?> 
  </body>
 </html>
