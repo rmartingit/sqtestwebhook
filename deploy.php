6d6419ab7b640d038f7905b7010c14acd4d34814
root <root@staging.c.sq2017-18-catjcbr.internal>
2018-04-05 15:59:45 +0000

    update deploy.php

--- deploy.php
+++ deploy.php
@@ -35,11 +35,7 @@
 </head>
 <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
 <pre>
- .  ____  .    ____________________________
- |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
- |___==___|  /              &copy; oodavid 2012 |
-              |____________________________|
+MCM-MC || Software Quality || PHP Git Deployment Script v0.1
 
 <?php echo $output; ?>
 </pre>
